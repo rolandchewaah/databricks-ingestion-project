6e071a7cd46a8b5fea0c3dfb96cee3d31c80c512
LAPTOP-TUDC1I2D\rollu <rolandchewaah@yahoo.com>
2026-02-19 23:22:45 -0500

    fifth commit

--- src/ingest_data.py
+++ src/ingest_data.py
@@ -1,4 +1,11 @@
 # Databricks notebook source
+import os
+from pyspark.sql import SparkSession
+
+# Optional: Use this to create separate cells in the UI
+# COMMAND ----------
+
+print("Starting ingestion...")
 input_path = "abfss://raw-data@mystorage.dfs.core.windows.net/uploads/"
 checkpoint_path = "abfss://checkpoints@mystorage.dfs.core.windows.net/ingestion_job/"
 
