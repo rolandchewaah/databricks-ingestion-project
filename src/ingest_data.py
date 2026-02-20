3612dec02299c77f93dc5d0ae293c52778aac74e
LAPTOP-TUDC1I2D\rollu <rolandchewaah@yahoo.com>
2026-02-19 22:25:13 -0500

    Initial commit

---
+++ src/ingest_data.py
@@ -0,0 +1,13 @@
+# src/ingest_data.py
+input_path = "abfss://raw-data@mystorage.dfs.core.windows.net/uploads/"
+checkpoint_path = "abfss://checkpoints@mystorage.dfs.core.windows.net/ingestion_job/"
+
+(spark.readStream
+  .format("cloudFiles")
+  .option("cloudFiles.format", "csv")
+  .option("cloudFiles.schemaLocation", checkpoint_path)
+  .load(input_path)
+  .writeStream
+  .option("checkpointLocation", checkpoint_path)
+  .trigger(availableNow=True)
+  .toTable("main.default.raw_ingested_data"))
