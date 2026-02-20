edabea634d3b356503801809a19898317dcc0a1c
LAPTOP-TUDC1I2D\rollu <rolandchewaah@yahoo.com>
2026-02-20 00:04:13 -0500

    nineth commit

--- src/ingest_data.py
+++ src/ingest_data.py
@@ -1,15 +1,25 @@
-# Databricks notebook source
 import os
-# ... rest of your code
-input_path = "abfss://raw-data@mystorage.dfs.core.windows.net/uploads/"
-checkpoint_path = "abfss://checkpoints@mystorage.dfs.core.windows.net/ingestion_job/"
+from pyspark.sql import SparkSession
 
-(spark.readStream
-  .format("cloudFiles")
-  .option("cloudFiles.format", "csv")
-  .option("cloudFiles.schemaLocation", checkpoint_path)
-  .load(input_path)
-  .writeStream
-  .option("checkpointLocation", checkpoint_path)
-  .trigger(availableNow=True)
-  .toTable("main.default.raw_ingested_data"))
+def main():
+    print("Starting ingestion...")
+    input_path = "abfss://raw-data@mystorage.dfs.core.windows.net/uploads/"
+    checkpoint_path = "abfss://checkpoints@mystorage.dfs.core.windows.net/ingestion_job/"
+
+    spark = SparkSession.builder.appName("IngestionJob").getOrCreate()
+
+    (spark.readStream
+        .format("cloudFiles")
+        .option("cloudFiles.format", "csv")
+        .option("cloudFiles.schemaLocation", checkpoint_path)
+        .load(input_path)
+        .writeStream
+        .option("checkpointLocation", checkpoint_path)
+        .trigger(availableNow=True)
+        .toTable("main.default.raw_ingested_data")
+    )
+
+    print("Ingestion job started.")
+
+if __name__ == "__main__":
+    main()
